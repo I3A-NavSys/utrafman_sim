a50681954ff08b933cf74f46b88f0a9a3b089b56
Jesús Jover <jesus.jover@uclm.es>
2022-07-13 20:14:58 +0200

    Init commit

---
+++ plugins/build/CMakeFiles/Project_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_generate_messages_eus"
+  "devel/share/roseus/ros/Project/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Project_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
