acb8b21fe25e9d748e52afe51903a00f047f5f2e
Jesús Jover <jesus.jover@uclm.es>
2022-08-15 08:58:14 +0200

    Added comments and changes to support init with Matlab

--- src/siam_god/src/god.cc
+++ src/siam_god/src/god.cc
@@ -31,8 +31,8 @@ namespace gazebo
             ros::Subscriber rosSub_remove;
 
             //Suscriptores para crear drones
-            ros::Subscriber rosSub_insert_drone;
-            ros::Subscriber rosSub_remove_drone;
+            //ros::Subscriber rosSub_insert_drone;
+            //ros::Subscriber rosSub_remove_drone;
 
             //Cola de mensajes
             ros::CallbackQueue rosQueue;
@@ -42,6 +42,7 @@ namespace gazebo
 
 
         public:
+            //Handle para la insercion de modelos por cada uno de los mensajes en el topico
             void insertModel(const std_msgs::String::ConstPtr& msg)
             {
                 ROS_INFO("Añadiendo drone con SDF [%s]", msg->data.c_str());
@@ -49,72 +50,79 @@ namespace gazebo
                 sdf::SDF sdf_object;
                 sdf::ElementPtr model_ptr;
 
-                //Establecemos el SDF del modelo con el contenido del topic
+                //Establecemos el SDF del modelo con el contenido del mensaje del topico
                 sdf_object.SetFromString(msg->data.c_str());
 
-                //Obtenemos el modelo
+                //Obtenemos el modelo insertado y cambiamos el nombre (no se hace de momento)
                 model_ptr = sdf_object.Root()->GetElement("model");
                 //model_ptr->GetAttribute("name")->SetFromString("drone-" + std::to_string(this->drones));
 
+                //Aumentamos el contador de drones
                 this->drones++;
+                //Insertamos el modelo en el mundo
                 this->parent->InsertModelSDF(sdf_object);
             }
 
             void removeModel(const std_msgs::String::ConstPtr& msg)
             {
                 ROS_INFO("Elimando drone-%s", msg->data.c_str());
+                //Eliminamos el modelo con el nombre indicado
                 this->parent->RemoveModel("drone_" + std::string(msg->data.c_str()));
             }
 
 
             void Load(physics::WorldPtr _parent, sdf::ElementPtr /*_sdf*/)
             {
-                //Almacenamos el puntero al elemento padre
+                //Almacenamos el puntero al elemento padre del mundo
                 this->parent = _parent;
 
-                //Reservamos espacio para los modelos
-                sdf::SDF box[N_SPHERES];
-                sdf::ElementPtr model[N_SPHERES];
+                //Para cargar un elemento de forma dinamica cuando se inicia la simulacion
+                    //Reservamos espacio para los modelos
+                    sdf::SDF box[N_SPHERES];
+                    sdf::ElementPtr model[N_SPHERES];
 
-                //Leemos el fichero con la plantilla del SDF del modelo
-                std::ostringstream fichero;
-                fichero << std::ifstream("/opt/ros/noetic/share/airspace-controller/models/sphere/sphere_template.txt").rdbuf();
-                std::string plantillaModelo = fichero.str();
+                    //Leemos el fichero con la plantilla del SDF del modelo
+                    std::ostringstream fichero;
+                    fichero << std::ifstream("/opt/ros/noetic/share/airspace-controller/models/sphere/sphere_template.txt").rdbuf();
+                    std::string plantillaModelo = fichero.str();
 
-                std::ostringstream ss;
-                std::string sdfString;
+                    std::ostringstream ss;
+                    std::string sdfString;
 
-                for (int i=0; i<N_SPHERES; i++)
-                {
-                    //Damos formato al modelo
-                    ss << boost::format(plantillaModelo) % i % i % i % (i*10);
-                    sdfString = ss.str();
-                    std::cout << ss.str() << std::endl;
+                    for (int i=0; i<N_SPHERES; i++)
+                    {
+                        //Damos formato al modelo
+                        ss << boost::format(plantillaModelo) % i % i % i % (i*10);
+                        sdfString = ss.str();
+                        std::cout << ss.str() << std::endl;
 
-                    //Asignamos el SDF al objeto
-                    box[i].SetFromString(sdfString);
-                    model[i] = box[i].Root()->GetElement("model");
-                    //Modificamos si atributo name por un valor unico
-                    model[i]->GetAttribute("name")->SetFromString("drone_" + std::to_string(i));
-                    //Añadimos el modelo al escenario
-                    _parent->InsertModelSDF(box[i]);
-                    this->drones++;
-                    //Reseteamos el contenido de ss
-                    ss.str("");
-                }
+                        //Asignamos el SDF al objeto
+                        box[i].SetFromString(sdfString);
+                        model[i] = box[i].Root()->GetElement("model");
+                        //Modificamos si atributo name por un valor unico
+                        model[i]->GetAttribute("name")->SetFromString("drone_" + std::to_string(i));
+                        //Añadimos el modelo al escenario
+                        _parent->InsertModelSDF(box[i]);
+                        this->drones++;
+                        //Reseteamos el contenido de ss
+                        ss.str("");
+                    }
 
-                //Informamos de que se ha terminado de cargar los modelos
-                ROS_INFO("Finalizado");
+                    //Informamos de que se ha terminado de cargar los modelos
+                    ROS_INFO("Finalizado");
+                //-----------------------------------------------------------
 
-                //Comprobamos si ROS ha sido inicializado
+                //Comprobamos si ROS ha sido inicializado, y lo iniciamos si no es asi
                 if (ros::isInitialized()){
                     int argc = 0;
                     char **argv = NULL;
                     ros::init(argc, argv, "airspaceGod");
                 }
+
                 //Creamos el nodo
                 this->rosNode = new ros::NodeHandle("god");
 
+                //Creamos el subscriptor handle para insertar modelos
                 ros::SubscribeOptions so_insert = ros::SubscribeOptions::create<std_msgs::String>(
                         "insert",
                         1000,
@@ -123,6 +131,7 @@ namespace gazebo
                         &this->rosQueue
                 );
 
+                //Creamos el subscriptor handle para eliminar modelos
                 ros::SubscribeOptions so_remove = ros::SubscribeOptions::create<std_msgs::String>(
                         "remove",
                         1000,
@@ -131,7 +140,7 @@ namespace gazebo
                         &this->rosQueue
                 );
 
-                //Creamos la suscripcion al nodo
+                //Creamos la suscripcion del nodo a los topicos
                 this->rosSub_insert = this->rosNode->subscribe(so_insert);
                 this->rosSub_remove = this->rosNode->subscribe(so_remove);
 
--- src/siam_main/matlab/ejecucion_paralela.m
+++ src/siam_main/matlab/ejecucion_paralela.m
@@ -1,25 +1,29 @@
 %Configuracion de la simulacion%
 
+%Numero de drones en la simulacion
+num_drone = 4;
+
+%Nombre del modelo de simulink
 model_name = "drone_control_tut3_R22a";
+%Nombre de los topicos
 pub_bus_command = model_name + "/drone simulator/command bus/pub_bus_command";
 sub_camera = model_name + "/drone simulator/camera bus/ROS communication/sub_camera";
 sub_odometry = model_name + "/drone simulator/imu bus/sub_odometry";
 
-num_drone = 4;
 
-
-% modelo = load_system(model_name);
-% model = getActiveConfigSet(model_name);
-
+%Apertura del modelo (para tenerlo cargado)
 open_system(model_name);
 
-% get_param(sub_odometry, "Topic")
-
+%Puesta en ejecucion de los controladores para los UAV
 for i = 0:1:(num_drone-1)
+    %Se genera una configuracion nueva de parametros de entrada del modelo
     model_config(i+1) = Simulink.SimulationInput(model_name);
+
+    %Modificacion de los nombres de los topicos en el modelo
     model_config(i+1) = model_config(i+1).setBlockParameter(pub_bus_command, "Topic", "/drone/"+i+"/bus_command");
     model_config(i+1) = model_config(i+1).setBlockParameter(sub_camera, "Topic", "/drone/"+i+"/onboard_camera/image_raw");
     model_config(i+1) = model_config(i+1).setBlockParameter(sub_odometry, "Topic", "/drone/"+i+"/odometry");
 end
 
+%Ejecucion de la simulacion de forma paralela
 out = parsim(model_config);
