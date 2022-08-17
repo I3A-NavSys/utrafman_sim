56388692ec42ec9aa9a1a8f83cb751cbb2f07ff9
siamsim <jesus.jover@uclm.es>
2022-08-17 10:03:45 +0200

    Finishing development of http server to manage SIAMsim

--- src/siam_main/http_server/app.js
+++ src/siam_main/http_server/app.js
@@ -5,26 +5,44 @@ const app = express()
 const port = 3000
 
 var simulation = false
+var closing = false
 
 app.get('/deploy/:launchfile', (req, res) => {
     var launchfile = req.params.launchfile
 
     if (simulation){
-        res.status(405).send("Process already in progress")
+    	if (closing){
+    		res.status(405).send("Previous simulation is closing...")
+    	} else {
+        	res.status(405).send("Simulation already in progress")
+        }
     } else {
-        simulation = spawn(`roslaunch siam_main ${launchfile}`);
+        simulation = spawn('roslaunch', ['siam_main', `${launchfile}`]);
         res.send(`Launching simulation with launchfile '${launchfile}' ...`)
     }
 })
 
 app.get('/destroy', (req, res) => {
+	if (closing){
+		res.status(405).send("Previous simulation is closing")
+		return;
+	}
     if (simulation) {
-        simulation = false
-        simulation.kill('SIGINT');
-        res.send('Finishing simulation...')
+    	closing = true
+    	simulation.on('close', (code, signal) => {
+    		simulation = false
+    		closing = false
+    		res.send(`Simulation has been finished. <br> Code: ${code} <br> Signal: ${signal}`)
+    	})
+    	simulation.kill('SIGINT');
     } else {
         res.status(405).send("No simulations in progress")
     }
+  })
+
+app.get('/reset', (req, res) => {
+	spawn('killall', ['-9', 'gzserver'])
+	res.send(`killall -9 gzserver has been executed`);
   })
 
 app.listen(port, () => {
