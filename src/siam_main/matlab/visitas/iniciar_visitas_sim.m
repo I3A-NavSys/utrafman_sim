458e592bb9330ea189d42cf204cbe57e7cffb224
Jesús Jover <jesus.jover@uclm.es>
2022-09-30 13:51:32 +0200

    Modificaciones sobre las visitas del i3a

--- src/siam_main/matlab/visitas/iniciar_visitas_sim.m
+++ src/siam_main/matlab/visitas/iniciar_visitas_sim.m
@@ -3,7 +3,8 @@ rosMasterIp = "192.168.1.131";
 rosMasterPort = 11311;
 
 %Modelos
-userDroneModel = fileread('../../models/dronechallenge_models/drone/model_template_2.sdf');
+userDroneModel = fileread('../../models/dronechallenge_models/drone/model_template_2_visitas.sdf');
+droneModel = fileread('../../models/dronechallenge_models/drone/model_template_1.sdf');
 
 %Conexion con el master de ROS
 try
@@ -17,10 +18,14 @@ end
 %Creacion del suscriptor para insertar los drones
 [rosInserterPub, rosInserterMsg] = rospublisher("/god/insert");
 
-for id=1:4
-    initPos = [0 -3+id 1];
+for id=1:5
+    initPos = [0 -0.6*id+1.8 1];
     %Definimos que SDF debe usarse y generamos el modelo del drone
-    droneSDF = sprintf(userDroneModel, id, initPos(1), initPos(2), initPos(3), id, id, id);
+    if id ~= 3
+    	droneSDF = sprintf(userDroneModel, id, initPos(1), initPos(2), initPos(3), id, id, id);
+    else
+    	droneSDF = sprintf(droneModel, id, initPos(1), initPos(2), initPos(3), id, id, id);
+    end
     
     %Generamos el mensaje y lo enviamos por el topico
     rosInserterMsg.Data = droneSDF;
