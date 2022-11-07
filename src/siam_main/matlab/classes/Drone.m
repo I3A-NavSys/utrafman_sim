bb3be3e24e57c81d5c75e0306656bff84ca7aeb0
jesusjoveruclm <jesus.jover@uclm.es>
2022-11-07 13:40:51 +0100

    Finished graficas2 script to view telemetry data and saved a test UTM file to load sim data

--- src/siam_main/matlab/classes/Drone.m
+++ src/siam_main/matlab/classes/Drone.m
@@ -12,7 +12,7 @@ classdef Drone < handle
 
         initLoc
         loc
-        locs
+        locs = ros.msggen.siam_main.Telemetry.empty(0);
 
         droneModel
         sdf
@@ -35,7 +35,7 @@ classdef Drone < handle
             obj.UTM = utm;
             obj.model = model;
             obj.initLoc = initLoc;
-            obj.locs = [0 0 0 0];
+            %obj.locs = [0 0 0 0];
         end
 
         %Generate SDF model to be used in Gazebo
@@ -55,26 +55,27 @@ classdef Drone < handle
         %Subcription to the drone odometry topic
         function obj = subToTelemety(obj)
             pause(1);
-            obj.ros_Telemetry_sub = rossubscriber(sprintf('/drone/%d/odometry', obj.droneId));
-            obj.timer_Upd_Telemetry = timer("Period", 5, "TimerFcn", @obj.updateLoc,"ExecutionMode","fixedRate");
+            obj.ros_Telemetry_sub = rossubscriber(sprintf('/drone/%d/telemetry', obj.droneId));
+            obj.timer_Upd_Telemetry = timer("Period", 1, "TimerFcn", @obj.updateLoc,"ExecutionMode","fixedRate");
             start(obj.timer_Upd_Telemetry);
         end
 
         %Subcription to the drone flight plan response 
         function obj = pubsubToFlightPlan(obj)
-            rospublisher(sprintf('/drone/%d/flightPlans/response', obj.droneId), "siam_main/FlightPlan");
-            obj.ros_flightPlans_pub = rospublisher(sprintf('/drone/%d/flightPlans/request', obj.droneId), "siam_main/FlightPlan");
-            obj.ros_flightPlans_sub = rossubscriber(sprintf('/drone/%d/flightPlans/response', obj.droneId));
-            obj.timer_Upd_Status = timer("Period", 2, "TimerFcn", @obj.updateStatus,"ExecutionMode","fixedRate");
-            start(obj.timer_Upd_Status);
+            %rospublisher(sprintf('/drone/%d/uplan', obj.droneId), "siam_main/Uplan");
+            obj.ros_flightPlans_pub = rospublisher(sprintf('/drone/%d/uplan', obj.droneId), "siam_main/Uplan");
+            %obj.ros_flightPlans_sub = rossubscriber(sprintf('/drone/%d/flightPlans/response', obj.droneId));
+            %obj.timer_Upd_Status = timer("Period", 2, "TimerFcn", @obj.updateStatus,"ExecutionMode","fixedRate");
+            %start(obj.timer_Upd_Status);
         end
 
         %Callback to update location of the drone
         function obj = updateLoc(obj, timer, time)
             [obj.ros_Telemetry_msg, status] = receive(obj.ros_Telemetry_sub, 0.2);
             if status
-                obj.loc = [obj.ros_Telemetry_msg.Pose.Position.X obj.ros_Telemetry_msg.Pose.Position.Y obj.ros_Telemetry_msg.Pose.Position.Z];
-                obj.locs(end+1,:) = [obj.UTM.Gclock obj.ros_Telemetry_msg.Pose.Position.X obj.ros_Telemetry_msg.Pose.Position.Y obj.ros_Telemetry_msg.Pose.Position.Z];
+                %obj.loc = [obj.ros_Telemetry_msg.Pose.Position.X obj.ros_Telemetry_msg.Pose.Position.Y obj.ros_Telemetry_msg.Pose.Position.Z];
+                %obj.locs(end+1,:) = [obj.UTM.Gclock obj.ros_Telemetry_msg.Pose.Position.X obj.ros_Telemetry_msg.Pose.Position.Y obj.ros_Telemetry_msg.Pose.Position.Z];
+                obj.locs(end+1) = obj.ros_Telemetry_msg;
             end
         end
 
--- src/siam_main/matlab/graficas.m
+++ src/siam_main/matlab/graficas.m
@@ -2,9 +2,20 @@ figure()
 hold all;
 view(3);
 leyenda = [];
+
 for i=1:size(drone,2)
+    x = zeros(1,size(drone(i).locs,2));
+    y = zeros(1,size(drone(i).locs,2));
+    z = zeros(1,size(drone(i).locs,2));
+
+    for j = 1:size(drone(i).locs,2)
+        x(j) = drone(i).locs(j).Pose.Position.X;
+        y(j) = drone(i).locs(j).Pose.Position.Y;
+        z(j) = drone(i).locs(j).Pose.Position.Z;
+    end
+
     co = sprintf('#%1d%1d0000',(9-i*2),(9-i*2));
-    plot3(drone(i).locs(:,2),drone(i).locs(:,3),drone(i).locs(:,4),'Color',co);
+    plot3(x,y,z,'Color',co);
     leyenda(1,i) = drone(1,i).droneId;
 end
 legend(string(leyenda))
