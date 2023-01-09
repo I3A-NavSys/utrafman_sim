047d6e218030e66bd5e86176c0da0a470f205312
jesusjoveruclm <jesus.jover@uclm.es>
2023-01-09 08:41:08 +0100

    New drone model added (solved rotor position error)

--- src/siam_main/matlab/ROSconfig.m
+++ src/siam_main/matlab/ROSconfig.m
@@ -1,6 +1,6 @@
 if getenv('USERNAME') == "jesus"
     ROS_IP = "1.0.0.131";
-    ROS_IP = "192.168.1.131";
+%     ROS_IP = "192.168.1.131";
 elseif getenv("USERNAME") == "usuario"
     ROS_IP = "192.168.1.131";
 elseif getenv('USERNAME') == "siamsim"
