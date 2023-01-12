e3e1b18f5d410233b2f34fbe05680d2723c151a8
jesusjoveruclm <jesusjmobile@gmail.com>
2023-01-12 16:06:47 +0100

    Removed matlab drom gazebo-ros

--- gazebo-ros/src/siam_main/matlab/ROSconfig.m
+++
@@ -1,8 +0,0 @@
-if getenv('USERNAME') == "jesus"
-    ROS_IP = "1.0.0.131";
-%     ROS_IP = "192.168.1.131";
-elseif getenv("USERNAME") == "usuario"
-    ROS_IP = "192.168.1.131";
-elseif getenv('USERNAME') == "siamsim"
-    ROS_IP = 'localhost';
-end
