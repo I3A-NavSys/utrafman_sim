89a63457897279653519f67f04a8b02d7db9856d
Jesús Jover <jesus.jover@alu.uclm.es>
2023-01-30 10:07:31 +0100

    Last changes to visitas

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/opt/ros/noetic/share/siam_sim/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
+underlay_workspaces = '/opt/ros/noetic/share/siam_sim/devel;/opt/ros/noetic'.split(';') if '/opt/ros/noetic/share/siam_sim/devel;/opt/ros/noetic' != '' else []
