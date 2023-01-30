edb39d41599ee5670562d38d126d3c2d0c6815f2
Jesús Jover <jesus.jover@alu.uclm.es>
2023-01-30 15:52:26 +0100

    Removed useless files and added some comments

--- src/matlab/ros_custom_message_compiler_MATLAB.m
+++ src/matlab/ros_custom_message_compiler_MATLAB.m
@@ -1,21 +1,33 @@
-if getenv('username') == "jesus"
+%This file contains the necessary code to compile ROS custom defined
+%messages to be used in MATLAB. If you use different machines to simulate,
+%you could define different configuration adding IF clausules.
+
+if getenv('username') == "jesus" %Jesus' personal computer
     addpath('C:\Users\usuario\Documents\i3a_repos\siam_sim\src\siam_main');
     py = pyenv('Version', 'C:\Users\jesus\AppData\Local\Programs\Python\Python39\python.exe');
     rosgenmsg('C:\i3a_repos\siam_sim\src\');
     addpath('C:\i3a_repos\siam_sim\src\matlab_msg_gen_ros1\win64\install\m')
 
-elseif getenv('username') == "usuario"
+elseif getenv('username') == "usuario" %I3A computer
     %Updated to repo refactor
     addpath('C:\Users\usuario\Documents\i3a_repos\siam_sim\src\gazebo-ros\src\siam_main');
     py = pyenv('Version', 'C:\Users\usuario\AppData\Local\Programs\Python\Python39\python.exe');
     rosgenmsg('C:\Users\usuario\Documents\i3a_repos\siam_sim\src\gazebo-ros\src\');
     addpath('C:\Users\usuario\Documents\i3a_repos\siam_sim\src\gazebo-ros\src\matlab_msg_gen_ros1\win64\install\m')
 
-elseif getenv('USERNAME') == "siamsim"
+elseif getenv('USERNAME') == "siamsim" %Virtual Machine
     addpath('..');
     py = pyenv('Version', '/usr/bin/python3.8');
     rosgenmsg('../..');
     addpath('../../matlab_msg_gen_ros1\win64\install\m')
+
+elseif getenv("USERNAME") == 'galadriel-ubuntu'
+    setenv("MY_PYTHON_VENV", "/tmp/venv");
+    ros.internal.createOrGetLocalPython(true);
+    addpath('../gazebo-ros/src/siam_main/');
+    py = pyenv('Version', '/usr/bin/python3.9');
+    rosgenmsg('../gazebo-ros/src/');
+    addpath('../gazebo-ros/src/siam_main/matlab_msg_gen_ros1\win64\install\m')
 end
 
 savepath
