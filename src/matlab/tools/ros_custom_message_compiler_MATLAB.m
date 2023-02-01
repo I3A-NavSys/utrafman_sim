3c4e72db1a4fd5e0e3a119474985c825b77a5b61
jesusjoveruclm <jesus.jover@uclm.es>
2023-02-01 12:30:13 +0100

    Removed files,, added comments to classes and restrutured

--- src/matlab/tools/ros_custom_message_compiler_MATLAB.m
+++ src/matlab/tools/ros_custom_message_compiler_MATLAB.m
@@ -9,7 +9,6 @@ if getenv('username') == "jesus" %Jesus' personal computer
     addpath('C:\i3a_repos\siam_sim\src\matlab_msg_gen_ros1\win64\install\m')
 
 elseif getenv('username') == "usuario" %I3A computer
-    %Updated to repo refactor
     addpath('C:\Users\usuario\Documents\i3a_repos\siam_sim\src\gazebo-ros\src\siam_main');
     py = pyenv('Version', 'C:\Users\usuario\AppData\Local\Programs\Python\Python39\python.exe');
     rosgenmsg('C:\Users\usuario\Documents\i3a_repos\siam_sim\src\gazebo-ros\src\');
