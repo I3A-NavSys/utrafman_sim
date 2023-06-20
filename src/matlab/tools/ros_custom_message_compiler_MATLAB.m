ff1fb117dfdb82218ff1d1f5d954e854919e8f80
Jesús Jover <jesus.jover@alu.uclm.es>
2023-06-20 11:30:02 +0200

    Tested MATLAB ROS compiler script and removed unnecesary code

--- src/matlab/tools/ros_custom_message_compiler_MATLAB.m
+++ src/matlab/tools/ros_custom_message_compiler_MATLAB.m
@@ -29,47 +29,6 @@ else ispc
     addpath(strcat(repo_path, 'src\gazebo-ros\src\matlab_msg_gen_ros1\win64\install\m'));
 end
 
-
-
-%Windows computer (Jesus' personal computer)
-% if getenv('username') == "jesus"
-%     addpath('C:\i3a_repos\utrafman_sim\src\gazebo-ros\src\utrafman_main');
-%     py = pyenv('Version', 'C:\Users\jesus\AppData\Local\Programs\Python\Python39\python.exe');
-%     rosgenmsg('C:\i3a_repos\utrafman_sim\src\gazebo-ros\src\');
-%     addpath('C:\i3a_repos\utrafman_sim\src\gazebo-ros\src\matlab_msg_gen_ros1\win64\install\m')
-% 
-% %Windows computer (I3A computer)
-% elseif getenv('username') == "usuario"
-%     addpath('C:\Users\usuario\Documents\i3a_repos\siam_sim\src\gazebo-ros\src\utrafman_main');
-%     py = pyenv('Version', 'C:\Users\usuario\AppData\Local\Programs\Python\Python39\python.exe');
-%     rosgenmsg('C:\Users\usuario\Documents\i3a_repos\siam_sim\src\gazebo-ros\src\');
-%     addpath('C:\Users\usuario\Documents\i3a_repos\siam_sim\src\gazebo-ros\src\matlab_msg_gen_ros1\win64\install\m')
-% 
-% %Ubuntu VM
-% elseif getenv('USERNAME') == "siamsim" %Virtual Machine
-%     addpath('../gazebo-ros/src/utrafman_main/');
-%     py = pyenv('Version', '/usr/bin/python3.9');
-%     rosgenmsg('../gazebo-ros/src/');
-%     addpath('../../matlab_msg_gen_ros1\win64\install\m')
-% 
-% %Ubuntu (galadriel computer)
-% elseif getenv('USERNAME') == "galadriel-ubuntu"
-%     setenv("MY_PYTHON_VENV", "/tmp/venv");
-%     ros.internal.createOrGetLocalPython(true);
-%     addpath('../gazebo-ros/src/utrafman_main/');
-%     py = pyenv('Version', '/usr/bin/python3.9');
-%     rosgenmsg('../gazebo-ros/src/');
-%     addpath('../gazebo-ros/src/matlab_msg_gen_ros1\win64\install\m')
-% 
-% elseif getenv('USERNAME') == "jesusjover"
-%     setenv("MY_PYTHON_VENV", "/tmp/venv");
-%     ros.internal.createOrGetLocalPython(true);
-%     addpath('../gazebo-ros/src/utrafman_main/');
-%     py = pyenv('Version', '/usr/bin/python3.9');
-%     rosgenmsg('../gazebo-ros/src/');
-%     addpath('/opt/ros/noetic/share/utrafman_sim/src/gazebo-ros/src/matlab_msg_gen_ros1/glnxa64/install/m')
-% end
-
 savepath
 clear classes
 rehash toolboxcache
