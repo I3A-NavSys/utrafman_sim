ea491b0b7271dbb33aa43fe5022fcc4f7783e5f8
Rafael Casado <rafael.casado@uclm.es>
2023-11-08 18:14:10 +0100

    update MATLAB ROS MSGS compilation

--- src/matlab/tools/ros_custom_message_compiler_MATLAB.m
+++ src/matlab/tools/ros_custom_message_compiler_MATLAB.m
@@ -1,14 +1,13 @@
 % This file contains the necessary code to compile ROS custom defined messages to be used in MATLAB. 
 
 clc; clear;
+
 UTRAFMAN_init;
 
-%Unix configuration
-if isunix
-    setenv("MY_PYTHON_VENV", "/tmp/venv");
-end
-
 rosgenmsg(fullfile(UTRAFMAN_DIR,'gazebo-ros/src/'));
-addpath(  fullfile(UTRAFMAN_DIR,'gazebo-ros/src/matlab_msg_gen_ros1/glnxa64/install/m'));
+
+addpath(fullfile(UTRAFMAN_DIR,'gazebo-ros/src/matlab_msg_gen_ros1/glnxa64/install/m'));
+
 clear classes
+
 rehash toolboxcache
